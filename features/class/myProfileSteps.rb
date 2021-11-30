585bbbcadc99d35a013d653e14c53daca28d1b8e
JoelEche20 <joel_ma_pa@gmail.com>
2021-11-30 17:56:09 -0400

    C-12 Added logic to automate edit profile.

---
+++ features/class/myProfileSteps.rb
@@ -0,0 +1,18 @@
+class MyProfileSteps
+	include RSpec::Matchers
+	include Capybara::DSL
+
+    def fillPersonalInformation(table)
+        data = table.rows_hash
+        data.each_pair do |key, value|
+            case key
+              when "Nombre:"
+                find('#field_1').set("Ucb Test")
+              when "Apellido:"
+                find('#field_2').set("Cal")
+              when "Nombre de Usuario:"
+                find('#field_3').set('UcbCal')
+            end
+        end
+    end
+end
